e950e0e17af0f86b5c34292d61b112263dbce222
Kidd <744900085@qq.com>
2023-03-11 09:27:31 +0000

    1. Use long haul blog theme

---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+export function formatDate(date) {
+    const d = new Date(date);
+    return d.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+}
+
+export function slugify(string) {
+    return string
+        .toString()
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+}
